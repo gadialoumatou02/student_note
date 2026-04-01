0518cda3c44e94601b24442e63a6236336de591d
gadialoumatou02 <gadialoumatou02@gmail.com>
2026-04-01 20:45:46 +0200

    data:ajout du modèle Student représentant une étudiante

---
+++ app/src/main/java/com/example/studentnote/data/Student.kt
@@ -0,0 +1,11 @@
+package com.example.studentnote.data
+
+data class Student(
+    val id: String,
+    val academie: String,
+    val discipline: String,
+    val competences: String,
+    val taux : Double
+)
+
+
